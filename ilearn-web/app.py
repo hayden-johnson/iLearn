eee7223d8ebf0bc10d95b9c9f04c87e5714602b0
Basel Hussein <57775754+thisisbasel@users.noreply.github.com>
2021-08-24 13:08:35 -0500

    updates to webapp

--- ilearn-web/app.py
+++ ilearn-web/app.py
@@ -7,11 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/goal", methods=["GET", "POST"])
-def goal():
-    goal = request.form.get("goal")
+# @app.route("/goal", methods=["GET", "POST"])
+# def goal():
+#     goal = request.form.get("goal")
 
-    return render_template("goal.html", goal=goal)
+#     return render_template("goal.html", goal=goal)
+
+@app.route("/climatechange")
+def climatechange():
+    return render_template("climatechange.html")
+
+
+@app.route("/machinelearning")
+def machinelearning():
+    return render_template("machinelearning.html")
+
+
+@app.route("/personalfinance")
+def personalfinance():
+    return render_template("personalfinance.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
